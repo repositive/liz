a1605676db2141aa64b7c57ae98417bc33e18a9a
danielgynn <danielgynn@hotmail.co.uk>
2015-11-27 10:48:51 +0000

    Basic blog structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,12 @@ var sass = require('gulp-sass');
 var minifycss = require('gulp-minify-css');
 
 gulp.task('styles', function() {
-  var options = {};
   gulp.src('./src/assets/sass/main.scss')
-    .pipe(sass(options).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(minifycss({keepSpecialComments: 0}))
     .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['styles'], function() {
   gulp.watch('./src/assets/sass/*.scss', ['styles']);
 });
