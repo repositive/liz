1ee7221edddc5d1a3ce12c4bfa3461c2e3fb5796
danielgynn <danielgynn@hotmail.co.uk>
2016-01-18 16:45:42 +0000

    Add Prism.js syntax highlighting

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifycss = require('gulp-minify-css');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifycss = require('gulp-minify-css');
 
-gulp.task('styles', function() {
-  gulp.src('./src/assets/sass/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(minifycss({keepSpecialComments: 0}))
-    .pipe(gulp.dest('./assets/css'));
+gulp.task('styles', function () {
+    gulp.src('./src/assets/sass/main.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(minifycss({keepSpecialComments: 0}))
+      .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('default', ['styles'], function() {
-  gulp.watch('./src/assets/sass/*.scss', ['styles']);
+gulp.task('default', ['styles'], function () {
+    gulp.watch('./src/assets/sass/*.scss', ['styles']);
 });
