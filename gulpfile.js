8bd719d48ab1b68a29dd527dca0856a8fd5404c5
danielgynn <danielgynn@hotmail.co.uk>
2016-03-29 10:04:03 +0100

    Move src to assets folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css');
 
 gulp.task('styles', function () {
-    gulp.src('./src/assets/sass/main.scss')
+    gulp.src('./assets/sass/main.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(minifycss({keepSpecialComments: 0}))
       .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('default', ['styles'], function () {
-    gulp.watch('./src/assets/sass/*.scss', ['styles']);
+    gulp.watch('./assets/sass/*.scss', ['styles']);
 });
