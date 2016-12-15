5fb53b111a804d38258185a7d7ecb0446e48f92a
Tom Byers <hello@tombye.rs>
2016-12-15 17:18:12 +0000

    Updating nav, footer & index styles

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css');
 
 gulp.task('styles', function () {
-    gulp.src('./assets/sass/main.scss')
+    gulp.src('./assets/scss/main.scss')
       .pipe(sass().on('error', sass.logError))
       .pipe(minifycss({keepSpecialComments: 0}))
       .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('default', ['styles'], function () {
-    gulp.watch('./assets/sass/*.scss', ['styles']);
+    gulp.watch('./assets/scss/*.scss', ['styles']);
 });
